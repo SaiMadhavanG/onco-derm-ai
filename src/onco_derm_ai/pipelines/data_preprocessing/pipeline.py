fd7cc20c0b3091619fac3decaaf3f117b312fe12
Srini2404 <krishnanmui@gmail.com>
2024-11-06 00:57:22 +0530

    data pre-process pipeline added

---
+++ src/onco_derm_ai/pipelines/data_preprocessing/pipeline.py
@@ -0,0 +1,25 @@
+"""
+This is a boilerplate pipeline 'data_preprocessing'
+generated using Kedro 0.19.8
+"""
+
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import normalizing_images, tensoring_resizing
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=normalizing_images,
+                inputs="train_raw",
+                outputs="train_intermediate",
+                name="normalizing_image_node"
+            ),
+            node(
+                func=tensoring_resizing,
+                inputs="train_intemediate",
+                outputs="pre-processed_train_data",
+                name="tensoring_resizing_node"
+            )
+        ]
+    )
