600494f88490e92d48a2e931b99aca68eb1819c2
SaiMadhavanG <g.saimadhavan@gmail.com>
2024-11-06 10:20:16 +0530

    fixed bugs in data preprocessing pipeline

--- src/onco_derm_ai/pipelines/data_preprocessing/pipeline.py
+++ src/onco_derm_ai/pipelines/data_preprocessing/pipeline.py
@@ -3,8 +3,10 @@ This is a boilerplate pipeline 'data_preprocessing'
 generated using Kedro 0.19.8
 """
 
-from kedro.pipeline import Pipeline, pipeline, node
+from kedro.pipeline import Pipeline, node, pipeline
+
 from .nodes import normalizing_images, tensoring_resizing
+
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -13,13 +15,13 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=normalizing_images,
                 inputs="train_raw",
                 outputs="train_intermediate",
-                name="normalizing_image_node"
+                name="normalizing_image_node",
             ),
             node(
                 func=tensoring_resizing,
-                inputs="train_intemediate",
+                inputs="train_intermediate",
                 outputs="pre-processed_train_data",
-                name="tensoring_resizing_node"
-            )
+                name="tensoring_resizing_node",
+            ),
         ]
     )
